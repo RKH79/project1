ea47bae9b322c5ba216dec63f5edfeec748d4349
Rohollah khoshhal <rohollah1703@gmail.com>
2019-10-07 00:30:13 +0330

    add files

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="Style/Style.css" rel="stylesheet" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+    <div id="Icon"><?= file_get_contents("icon/book.svg") ?></div>
+    <div id="Links">
+        <a class="Link" href="#">Home</a>
+        <a class="Link" href="#">Contact US</a>
+        <a class="Link" href="#">About US</a>
+    </div>
+</nav>
+<div>
+    <aside>
+        <div id="News">
+            <div class="AsideTitle">title</div>
+            <ul class="AsideText">
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+            </ul>
+        </div>
+        <div id="News">
+            <div class="AsideTitle">title</div>
+            <ul class="AsideText">
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+            </ul>
+        </div>
+        <div id="News">
+            <div class="AsideTitle">title</div>
+            <ul class="AsideText">
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+                <li><a href="#">news1</a></li>
+            </ul>
+        </div>
+    </aside>
+    <comment></comment>
+</div>
+
+</body>
+</html>
