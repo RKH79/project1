21da14c91636e733be1754b2283c8c11dc712a80
RKH79 <rohollah1703@gmail.com>
2019-12-25 02:38:08 +0330

    test2

--- Script/Script.js
+++ Script/Script.js
@@ -1,15 +1,16 @@
 let slideBox = document.querySelectorAll("#slideBox .slide");
-slideBox[0].style.display='block';
+slideBox[0].style.display = 'block';
 let activeSlide = 0;
+
 function slide() {
-    activeSlide ++;
+    activeSlide++;
     if (activeSlide > 4)
         activeSlide = 0;
-    if (activeSlide===0)
+    if (activeSlide === 0)
         slideBox[4].style.display = 'none';
     else
-        slideBox[activeSlide-1].style.display = 'none';
+        slideBox[activeSlide - 1].style.display = 'none';
     slideBox[activeSlide].style.display = 'block';
     console.log(activeSlide);
 }
-setInterval(slide(),1000);
+setInterval(slide(), 1000);
